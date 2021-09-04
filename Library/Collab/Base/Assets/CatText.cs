f15e63add8c6bd36f50da54ee9d45dd2d6576c2f
thispimilx <wachirayana.wan@student.mahidol.ac.th>
2021-09-05 06:13:43 +0700

    google cardboard android

---
+++ Library/Collab/Base/Assets/CatText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatText : MonoBehaviour
+{
+    public int scoreToWin = 3;
+    public Text myText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "finish")
+        {
+            if (FishScore.scoreValue >= scoreToWin)
+            {
+                myText.text = "Win!";
+            }
+        }      
+    }
+}
